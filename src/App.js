ccbb002c7114b7606bf62db10fdf8ac31a0771b4
hiswe <hiswehalya@gmail.com>
2019-02-20 16:04:03 +0100

    APP – add some basic error boundary

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import ShopList from './shop-list'
+
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error) {
+    console.log(error)
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true }
+  }
+
   render() {
+    const { state } = this
+    const { hasError } = state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {hasError ? <h1>Something went wrong.</h1> : <ShopList />}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
